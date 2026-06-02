f021d676c645e9a9c20847494010dd4c3001a42e
iplaynasty <amansharma1841112016@gmail.com>
2026-06-02 14:02:03 +0530

    Update lineage_Spacewar.mk

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -15,9 +15,7 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Lunaris flags
-LUNARIS_BUILD_TYPE := OFFICIAL
-WITH_GMS := true
+
 TARGET_CUSTOM_UDFPS := true
 TARGET_OPTIMIZED_DEXOPT := true
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -37,3 +35,33 @@ PRODUCT_GMS_CLIENTID_BASE := android-nothing
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=Spacewar \
     BuildFingerprint="Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1666412462:user/release-keys"
+
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,50
+AXION_CAMERA_FRONT_INFO := 16
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := iplaynasty
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_778G+
+
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_INCLUDE_AXFX := true
+
+add in product overrides or set using console: adb shell setprop persist.sys.ax_debug_enabled 1 
+persist.sys.ax_debug_enabled=1
+
+TARGET_DISABLES_LIBPERF ?= false
+
+# Enable or disable ScrollOptimizer globally
+persist.sys.perf.scroll_opt = true
+
+# Heavy app handling mode
+# 0 - Disable heavy app classification
+# 1 - Enable dynamic detection (based on frame duration and buffer load)
+# 2 - Treat all apps as heavy for performance
+persist.sys.perf.scroll_opt.heavy_app = 1
+
